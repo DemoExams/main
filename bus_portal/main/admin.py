710ea630214f112d382879abce1cd7514635649d
DemoExams <leshkalepeshka15@gmail.com>
2026-05-21 13:42:34 +0300

    commit 3
    Добавил админку слайдер добавил немного стилей

--- bus_portal/main/admin.py
+++ bus_portal/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Application
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'full_name', 'email', 'phone']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['username', 'full_name']
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'course_name', 'start_date', 'status', 'created_at']
+    list_filter = ['status', 'course_name', 'payment_method']
+    search_fields = ['user__username', 'user__full_name']
+    list_editable = ['status']
